66156f3205e624941d6cac5b47b142172b933b5f
lxfontes <lxfontes@gmail.com>
2014-11-12 14:52:22 -0500

    fixing hash collision

--- unicorno.go
+++ unicorno.go
@@ -31,8 +31,11 @@ func (i *inFlightRequest) String() string {
 	return fmt.Sprintf("[%s] http://%s%s", i.req.Method, i.req.Host, i.req.RequestURI)
 }
 
+type PortHash map[uint64]*inFlightRequest
+type IpHash map[uint64]PortHash
+
 type requestPool struct {
-	inFlight map[uint64]*inFlightRequest
+	inFlight IpHash
 	mtx      sync.Mutex
 }
 
@@ -43,38 +46,57 @@ func (p *requestPool) RemoveInFlight() {
 	removeQueue := []uint64{}
 
 	cutOff := time.Now().Add(-1 * time.Minute)
-	for hash, req := range p.inFlight {
-		if req.startTime.Before(cutOff) {
-			removeQueue = append(removeQueue, hash)
+	for _, flowMap := range p.inFlight {
+		for hash, req := range flowMap {
+			if req.startTime.Before(cutOff) {
+				removeQueue = append(removeQueue, hash)
+			}
 		}
-	}
 
-	for _, hash := range removeQueue {
-		delete(p.inFlight, hash)
+		for _, hash := range removeQueue {
+			delete(flowMap, hash)
+		}
 	}
 }
 
-func (p *requestPool) PutReq(flow gopacket.Flow, req *http.Request, when time.Time) {
+func (p *requestPool) PutReq(net gopacket.Flow, transport gopacket.Flow, req *http.Request, when time.Time) {
 	p.mtx.Lock()
 	defer p.mtx.Unlock()
 	r := &inFlightRequest{
 		req:       req,
 		startTime: when,
 	}
-	p.inFlight[flow.FastHash()] = r
+
+	netHash := net.FastHash()
+	portHash := transport.FastHash()
+
+	_, ok := p.inFlight[netHash]
+	if !ok {
+		p.inFlight[netHash] = make(PortHash)
+	}
+	p.inFlight[netHash][portHash] = r
 }
 
-func (p *requestPool) PutResp(flow gopacket.Flow, resp *http.Response, when time.Time) {
+func (p *requestPool) PutResp(net gopacket.Flow, transport gopacket.Flow, resp *http.Response, when time.Time) {
 	p.mtx.Lock()
 	defer p.mtx.Unlock()
-	req, ok := p.inFlight[flow.FastHash()]
+
+	netHash := net.FastHash()
+	portHash := transport.FastHash()
+
+	flowMap, ok := p.inFlight[netHash]
+	if !ok {
+		return
+	}
+
+	req, ok := flowMap[portHash]
 	if !ok {
 		return
 	}
 
 	deltaTime := when.Sub(req.startTime).String()
 	fmt.Println(req, "->", resp.Status, ":", deltaTime)
-	delete(p.inFlight, flow.FastHash())
+	delete(flowMap, portHash)
 }
 
 type srcStreamFactory struct {
@@ -119,7 +141,7 @@ func (d *dstStreamFactory) run(h *tcpStream) {
 		return
 	}
 	defer req.Body.Close()
-	d.pool.PutReq(h.net, req, startTime)
+	d.pool.PutReq(h.net, h.transport, req, startTime)
 	tcpreader.DiscardBytesToFirstError(buf)
 }
 
@@ -131,7 +153,7 @@ func (s *srcStreamFactory) run(h *tcpStream) {
 		return
 	}
 	tcpreader.DiscardBytesToFirstError(buf)
-	s.pool.PutResp(h.net, resp, time.Now())
+	s.pool.PutResp(h.net, h.transport, resp, time.Now())
 }
 
 func runFilter(factory tcpassembly.StreamFactory, iface string, snaplen int32, filter string, ch chan os.Signal) {
@@ -175,7 +197,7 @@ func main() {
 	signal.Notify(ch, os.Interrupt, os.Kill)
 
 	reqPool := &requestPool{
-		inFlight: make(map[uint64]*inFlightRequest),
+		inFlight: make(IpHash),
 	}
 
 	srcFactory := &srcStreamFactory{pool: reqPool}
